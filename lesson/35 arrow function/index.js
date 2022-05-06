521b17ec31b3f90ae03707f029eec5858d39df22
lmxy <wjzxyaowei@163.com>
2022-05-06 17:37:52 +0800

    learned 2 hours

---
+++ lesson/35 arrow function/index.js
@@ -0,0 +1,10 @@
+// arrow function = compact alternative to a traditional function
+//      =>
+
+let grades = [100, 50, 90, 60, 80, 70];
+
+grades.sort((x, y) => x - y);
+grades.forEach((element) => console.log(element));
+
+
+
