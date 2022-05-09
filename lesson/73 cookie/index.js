f4132343774592115bdd6dfeac7707901573452f
Administrator <wjzxyaowei@gmail.com>
2022-05-09 22:10:18 +0800

    learned more a hour 5/9/2022

--- lesson/73 cookie/index.js
+++ lesson/73 cookie/index.js
@@ -1,20 +1,42 @@
-// cookie = a small text file stored on your computer
-//          used to remember information about the user
-//          saved in name=value pairs
+const firstText = document.querySelector("#firstText");
+const lastText = document.querySelector("#lastText");
+const submitBtn = document.querySelector("#submitBtn");
+const cookieBtn = document.querySelector("#cookieBtn");
 
-// console.log(navigator.cookieEnabled);
-// document.cookie = "firstName=Ptrick; expires=Sun, 1 January 2030 12:00:00 UTC; path=/";
-// document.cookie = "lastName=SquarePants; expires=Sun, 1 January 2000 12:00:00 UTC; path=/";
+submitBtn.addEventListener("click", () => {
+  setCookie("firstName", firstText.value, 365);
+  setCookie("lastName", lastText.value, 365);
+});
+cookieBtn.addEventListener("click", () => {
+  firstText.value = getCookie("firstName");
+  lastText.value = getCookie("lastName");
+});
 
-// console.log(document.cookie);
+// deleteCookie("firstName");
+// deleteCookie("lastName");
 
-setCookie("email", "Spong@gmial.com", 365);
-
-console.log(document.cookie);
 
 function setCookie(name, value, daysTolive){
-  const date = new Date();
-  date.getTime(date.getTime() + (daysTolive * 24 * 60 * 60 * 1000));
-  let expires = "expires=" + date.toUTCString();
+  const date= new Date();
+  date.getTime(date.getTime() + daysTolive * 24 * 60 * 60 * 1000);
+  let expires = "expires" + date.toUTCString();
   document.cookie = `${name}=${value}; ${expires}; path=/`
+  // console.log(document.cookie)
+}
+function deleteCookie(name){
+  setCookie(name, null, null);
+}
+function getCookie(name){
+  const cDecoded = decodeURIComponent(document.cookie);
+  // console.log(cDecoded);
+  const cArray = cDecoded.split("; ");
+  // console.log(cArray);
+  let result = null;
+
+  cArray.forEach(element => {
+    if(element.indexOf(name) == 0){
+      result = element.substring(name.length + 1);
+    }
+  })
+  return result;
 }
